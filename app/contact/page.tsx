e1f888d8f030af9a253abf825cad4a9afcfdfbc2
dialite <raymondolisa01@gmail.com>
2025-05-21 14:42:06 +0100

    Fix lint errors

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -101,7 +101,14 @@ export default function ChurchContactForm() {
 
   return (
     <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Navbar />
+      <Navbar
+        scrollToWebsiteDesign={() => {}}
+        scrollToGraphicDesign={() => {}}
+        scrollToShopifyStores={() => {}}
+        // scrollToBrands={() => {}}
+        // scrollToServices={() => {}}
+      />
+
       <div className="md:flex items-start justify-center md:py-20 px-6 mt-12">
         <div>
           <h1 className="text-5xl font-medium w-full md:w-2/3 pb-5 md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
